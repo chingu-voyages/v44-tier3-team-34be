9a5230aa5b8ffd936d32e50bc01b7dca310c438d
CCMatson <catherine.matson@gmail.com>
2023-05-27 19:23:08 -0700

    delete reactions on posts

--- routes/posts.js
+++ routes/posts.js
@@ -14,14 +14,18 @@ router.get('/:id', checkAuth, postsCtrl.show)
 
 router.post('/', checkAuth, postsCtrl.create)
 router.post('/:id/comments', checkAuth, postsCtrl.createComment)
+//reactions and "likes"
 router.post('/:id/reactions', checkAuth, postsCtrl.createReaction)
 
 router.put('/:id', checkAuth, postsCtrl.update)
 
 router.put('/:postId/comments/:commentId', checkAuth, postsCtrl.updateComment)
+
 router.delete('/:id', checkAuth, postsCtrl.delete)
 //the author of the post and the comment can delete the comment
 router.delete('/:postId/comments/:commentId', checkAuth, postsCtrl.deleteComment)
+
+router.delete('/:postId/reactions/:reactionId', checkAuth, postsCtrl.deleteReaction)
 
 
 
